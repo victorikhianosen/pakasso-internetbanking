4ab2238623adac13f37a3794de5084ddca4e3d81
Adejumobi Ayotomiwa Samuel <adejusamuel@gmail.com>
2026-03-05 12:53:48 +0100

    updated api call to logout on error code 401

--- lib/api/get-balance.ts
+++ lib/api/get-balance.ts
@@ -1,15 +1,32 @@
+import { UseUser } from "@/context/UserContext";
+import { serverLogout } from "../../app/actions/auth/login/logout.action";
+
+ const logout = () => {
+  localStorage.removeItem("access_token");
+  window.location.href = "/login";
+  const {clearUser} = UseUser();
+  clearUser();
+  serverLogout()
+};
+
 export async function fetchBalance() {
   const res = await fetch("/api/balance", {
-    headers: {
-      Accept: "application/json",
-    },
-    credentials: "include", // sends cookies automatically
+    headers: { Accept: "application/json" },
+    credentials: "include",
   });
+
+  // Status-based logout (PRIMARY)
+  if (res.status === 401 || res.status === 403) {
+    logout();
+    throw new Error("Session expired");
+  }
+
+  //  Parse response ONCE
+  const data = await res.json();
 
   if (!res.ok) {
     throw new Error("Failed to fetch balance");
   }
-  
 
-  return res.json();
+  return data;
 }
